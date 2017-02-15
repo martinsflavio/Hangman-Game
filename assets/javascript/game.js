eaa6ae0789140691aef91f80b954765931315557
Flavio dos Reis Martins <flaviodosreismartins@gmail.com>
2017-02-15 02:20:14 -0800

    js an html done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,53 @@
 //-----------Global Variables---------------
-var choises = ["brazil","australia"]
-var win = 0; 
-var lose = 0;
-var nGuess = 10;
-var wrongGuess = [];
-var word = [];
+var choises=["brazil","australia"]
+var win=0, lose=0; 
+var nGuess=10;
+var wrongGuess=[], word=[], setupSR=[], compGuess=[];
+var userGuess="";
 
-function printSpace(nSpace){
-	var result = [];
-	for(var i = 0; i < nSpace.length ;i++){
-		result[i] = "-";
+//Generate the random word and the array full of '-'
+function newGame(choisesArray){
+	var spaces = [];
+	var randomWord = choisesArray[Math.floor(Math.random() * choisesArray.length)].split("");
+		for(var i = 0; i < randomWord.length ;i++){
+			spaces[i] = "-";
+	}
+	return [spaces,randomWord];
+}
+function printRightGuess(letter,wToCompare,result){
+	for(var i = 0; i < wToCompare.length ;i++){
+		if (letter === wToCompare[i]){
+			result[i] = letter;
+		}
 	}	
 	return result;
-}
-function printRightGuess(caracter,wToCompare,output){
-	for(var i = 0; i < wToCompare.length ;i++){
-		if (caracter === wToCompare[i]){
-			output[i] = caracter;
-		}
-	}
-	return output;
 }
 function compare(element) { 
   return element !== userGuess; 
 } 
 function checkWin(array1,array2){
-	array1 = array1.join("");
-	array2 = array2.join("");
+	var array1 = array1.join("");
+	var array2 = array2.join("");
 	if (array1 === array2){
 		return true;
 	}else{
 		return false;
 	}
+	console.log(array1);
+	console.log(array2);
 }
-function newGame(){
-	nGuess = 10;
-	wrongGuess = [];
-	word = [];
-}
+
 function printScreen(){
+	document.getElementById("word").innerHTML = word.join(" ");
+	document.getElementById("wrong-guess").innerHTML = wrongGuess;
+	document.getElementById("n-guesses").innerHTML = nGuess;
+	document.getElementById("times-win").innerHTML = win;
+	document.getElementById("times-lose").innerHTML = lose;
+}
+function reset(){
+	nGuess=10;
+	wrongGuess=[], word=[],compGuess=[];
+	userGuess="";
 	document.getElementById("word").innerHTML = word.join(" ");
 	document.getElementById("wrong-guess").innerHTML = wrongGuess;
 	document.getElementById("n-guesses").innerHTML = nGuess;
@@ -47,33 +56,35 @@ function printScreen(){
 }
 
 
-document.getElementById("press-start").innerHTML = "Press any key to get started!";
-document.onkeyup = function(event){
-	compGuess = choises[Math.floor(Math.random() * choises.length)].split("");
-	word = printSpace(compGuess);
-	document.getElementById("press-start").innerHTML = "Chose a Letter";
+setupSR = newGame(choises);
+document.onkeyup = function(event){ 
+	userGuess = event.key.toLowerCase();
+	compGuess = setupSR[1];
+	word = setupSR[0]; 
 
-	document.onkeyup = function(event){
-		userGuess = event.key.toLowerCase();
 	
-		if (compGuess.every(compare)){
-			if (wrongGuess.every(compare)){
-				wrongGuess.push(userGuess);
-				nGuess--;
-			}	
-		}else{
-			word = printRightGuess(userGuess,compGuess,word);
-		}
-
-		if (checkWin(compGuess,word)){
-			win++;		
-		}else if(nGuess === 0){
-			lose++;
-		}
-		printScreen();
+	if (compGuess.every(compare)){
+		if (wrongGuess.every(compare)){
+			wrongGuess.push(userGuess);
+			nGuess--;
+		}	
+	}else{
+		word = printRightGuess(userGuess,compGuess,word);
 	}
-}
 
+	if (checkWin(compGuess,word)){
+		win++;
+		setupSR = newGame(choises);
+		reset();
+	}else if(nGuess === 0){
+		lose++;
+		setupSR = newGame(choises);
+		reset();
+	}
+
+	printScreen();
+	console.log(word);
+}
 
 
 
